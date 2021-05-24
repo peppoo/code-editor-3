e575f6e9aa26390b8206479da6e57ed76624323f
peppoo <peppo3014@i.softbank.jp>
2021-05-24 20:30:21 +0900

    First commit

---
+++ local-api.js
@@ -0,0 +1,10 @@
+// import exporess
+
+//  one route
+//  serve up react production assets
+
+//  Save a list of cells into a file
+
+// Load  up a list of cells from a file
+
+//  express sever listen to port 4050
---
+++ src/components/cell-list.tsx
@@ -0,0 +1,33 @@
+import './cell-list.css';
+import { Fragment,useEffect } from 'react';
+import { useTypedSelector } from '../hooks/use-typed-selector';
+import CellListItem from './cell-list-item';
+import AddCell from './add-cell';
+import { useActions } from '../hooks/use-actions';
+ 
+const CellList: React.FC = () => {
+  const cells = useTypedSelector(({ cells: { order, data } }:any) =>
+    order.map((id:any) => data[id])
+  );
+  const { fetchCells } = useActions();
+
+  useEffect(() => {
+    fetchCells();
+  }, []);
+
+  const renderedCells = cells.map((cell: any) => (
+    <Fragment key={cell.id}>
+      <CellListItem cell={cell} />
+      <AddCell previousCellId={cell.id} />
+    </Fragment>
+  ));
+
+  return (
+    <div className="cell-list">
+      <AddCell forceVisible={cells.length === 0} previousCellId={null} />
+      {renderedCells}
+    </div>
+  );
+};
+
+export default CellList;
